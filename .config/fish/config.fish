ee9f15b9bf1d0c59c19dcd0dae1d82ffc39cdfab
RodsonMatos <rodsonmatos@tuta.com>
2025-08-26 20:08:58 -0300

    Do I have to detail this?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,9 +22,9 @@ export EDITOR="/usr/bin/nvim"
 export VISUAL="/usr/bin/nvim"
 export BW_SESSION="SheIVvLF4FkUt+CC9fVbB3UZlysDp10PTXM8cDhSzLbhsw5XNGMoRcQLLjWSsbBVeoz3tYqIJo3mGGGtqSS7Ug=="
 
-zoxide init fish | source
-
 source ~/.config/fish/themes/color-fish.fish
 
 function fish_greeting
 end
+
+zoxide init fish | source
--- .config/fish/themes/color-fish.fish
+++ .config/fish/themes/color-fish.fish
@@ -1,34 +1,34 @@
 # name: 'Base16-ansi'
-# preferred_background: 241D3E
+# preferred_background: 1B0B0D
 # based on: 'https://github.com/chriskempson/base16-default-schemes'
 
-set fish_color_normal BCD162
-set fish_color_autosuggestion 8D9D4A
-set fish_color_cancel 8D9D4A
-set fish_color_command BFACDE
-set fish_color_comment 7D769D
-set fish_color_cwd 8D9D4A
-set fish_color_cwd_root 9587AB
-set fish_color_end BCD162
-set fish_color_error 7A86C9
-set fish_color_escape BFACDE
-set fish_color_history_current B3A8E0
-set fish_color_host 7F73B3
-set fish_color_host_remote 8D9D4A
-set fish_color_keyword 7F73B3
-set fish_color_operator 917FDA
-set fish_color_option 8D9D4A
-set fish_color_param 6D87C4
-set fish_color_quote B3A8E0
-set fish_color_redirection 8D9D4A
-set fish_color_search_match white --background=241D3E --bold
-set fish_color_selection BCD162 --background=B3A8E0 --bold
-set fish_color_status 7F73B3
-set fish_color_user C6CDA9
-set fish_color_valid_path BCD162 --underline
-set fish_pager_color_completion 7D769D
-set fish_pager_color_description B3A06D 8D9D4A
+set fish_color_normal 637379
+set fish_color_autosuggestion 704F52
+set fish_color_cancel 704F52
+set fish_color_command BA6962
+set fish_color_comment A8A59B
+set fish_color_cwd 59656A
+set fish_color_cwd_root A16E68
+set fish_color_end 82565B
+set fish_color_error 718684
+set fish_color_escape BA6962
+set fish_color_history_current F0ECDE
+set fish_color_host B6AF9A
+set fish_color_host_remote 704F52
+set fish_color_keyword B6AF9A
+set fish_color_operator F3EACE
+set fish_color_option 59656A
+set fish_color_param 9A7C68
+set fish_color_quote F0ECDE
+set fish_color_redirection 59656A
+set fish_color_search_match white --background=1B0B0D --bold
+set fish_color_selection 637379 --background=F0ECDE --bold
+set fish_color_status B6AF9A
+set fish_color_user 98A1A0
+set fish_color_valid_path 637379 --underline
+set fish_pager_color_completion A8A59B
+set fish_pager_color_description B3A06D 704F52
 set fish_pager_color_prefix normal --bold --underline
 set fish_pager_color_progress brwhite --background=cyan --bold
-set fish_pager_color_selected_background --background=B3A8E0
-set fish_pager_color_background D0C8EF
+set fish_pager_color_selected_background --background=F0ECDE
+set fish_pager_color_background FBF8EE
